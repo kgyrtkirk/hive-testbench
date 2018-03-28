39f72c2437e407c9ab15d9c5e12fb9a19452ceb9
Zoltan Haindrich <kirk@rxd.hu>
2018-03-28 12:10:47 +0000

    add a1

---
+++ sample-queries-tpcds/a1.sql
@@ -0,0 +1,24 @@
+set hive.map.aggr=true;
+set mapreduce.reduce.speculative=false;
+set hive.auto.convert.join=true;
+set hive.optimize.reducededuplication.min.reducer=1;
+-- set hive.optimize.mapjoin.mapreduce=true;
+set hive.stats.autogather=true;
+set mapred.reduce.parallel.copies=30;
+set mapred.job.shuffle.input.buffer.percent=0.5;
+set mapred.job.reduce.input.buffer.percent=0.2;
+set mapred.map.child.java.opts=-server -Xmx2800m -Djava.net.preferIPv4Stack=true;
+set mapred.reduce.child.java.opts=-server -Xmx3800m -Djava.net.preferIPv4Stack=true;
+set mapreduce.map.memory.mb=3072;
+set mapreduce.reduce.memory.mb=4096;
+set hive.llap.execution.mode=only;
+
+select 'oko';
+
+set hive.query.results.cache.enabled=false;
+
+set hive.query.reexecution.always.collect.operator.stats=true;
+
+set hive.query.reexecution.enabled=false;
+
+set hive.mapjoin.hybridgrace.hashtable=false;
